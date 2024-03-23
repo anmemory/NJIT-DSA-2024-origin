29fe92b522f7fe572e6869119dfc34f760dc0d40
anmemory <126429334+anmemory@users.noreply.github.com>
2024-03-23 23:09:31 +0800

    1

--- 01-arrays/src/main/java/oy/tol/tra/Algorithms.java
+++ 01-arrays/src/main/java/oy/tol/tra/Algorithms.java
@@ -33,7 +33,7 @@ public class Algorithms {
     private static <T> void swap(T[] array, int i, int j) {
         T temp = array[i];
         array[i] = array[j];
-        array[j] = temp;
+        array[j] = temp;//
     }
 }
 
